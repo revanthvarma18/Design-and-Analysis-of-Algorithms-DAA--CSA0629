e10be2f0a2f9a53c38a3219679e98368bdb4e966
Revanth Alli <112714751+revanthvarma18@users.noreply.github.com>
2024-12-26 13:24:40 +0530

    Add files via upload

---
+++ All-Pairs Shortest Path Using Floyd's Algorithm.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+#define INF 99999
+#define V 4
+
+void floydWarshall(int graph[V][V]) {
+    int dist[V][V];
+    for (int i = 0; i < V; i++)
+        for (int j = 0; j < V; j++)
+            dist[i][j] = graph[i][j];
+
+    for (int k = 0; k < V; k++) {
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j])
+                    dist[i][j] = dist[i][k] + dist[k][j];
+            }
+        }
+    }
+
+    printf("Shortest Distances Between Every Pair of Vertices:\n");
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            if (dist[i][j] == INF)
+                printf("%7s", "INF");
+            else
+                printf("%7d", dist[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int graph[V][V] = {
+        {0, 8, 7, 8},
+        {9, 0, 11, 12},
+        {10, 9, 0, 11},
+        {8, 10, 11, 0}
+    };
+
+    floydWarshall(graph);
+    return 0;
+}
+
